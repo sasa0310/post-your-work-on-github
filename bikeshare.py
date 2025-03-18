a2ebfeebee17fd085c4618781c07135f3459a7ef
sasa0310 <Xiuping.Li@bmw-brilliance.cn>
2025-03-18 06:33:40 +0000

    Improve function naming for bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-def get_filters():
+def get_user_filters():
     """
     Asks user to specify a city, month, and day to analyze.
 
